c35d14d7d6f7eb60f0d07e983ed76e4eea94a5c1
jmeaster30 <jhneasterday09@gmail.com>
2020-05-25 19:03:27 -0400

    okay i think we are good to rock and roll

--- nav_buttons.js
+++ nav_buttons.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
   var border_color_d = style.getPropertyValue('--border-color-down');
   var button_color_u = style.getPropertyValue('--button-color-up');
   var button_color_d = style.getPropertyValue('--button-color-down');
+  var url_start = location.href.match(/[\s\S]*.io($|\/)/)[0];
+  if(url_start[url_start.length - 1] != '/')
+    url_start = url_start + '/';
 
   //set navigation buttons
   $(".my_nav_btn")
@@ -24,19 +27,19 @@ $(document).ready(function(){
   //home
   $("#home_btn").click(function(){
     console.log("home button clicked");
-    window.location = "index.html";
+    window.location = url_start + "index.html";
   });
 
   //repos
   $("#repos_btn").click(function(){
     console.log("repositories clicked");
-    window.location = "repo_page.html";
+    window.location = url_start + "repo_page.html";
   });
 
   //demos
   $("#demos_btn").click(function(){
     console.log("demos button clicked");
-    window.location = "demo_page.html";
+    window.location = url_start + "demo_page.html";
   });
 
   //resume
