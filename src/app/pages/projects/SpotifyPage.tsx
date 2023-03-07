bcf9f703d07dfd265e028eb5d52bcf271b4ee6c1
jmeaster30 <jhneasterday09@gmail.com>
2023-03-06 22:59:13 -0500

    Adding changes

--- src/app/pages/projects/SpotifyPage.tsx
+++ src/app/pages/projects/SpotifyPage.tsx
@@ -2,11 +2,10 @@ import { observer } from "mobx-react";
 import React from "react";
 import styles from '../../styles/spotifyStyles.scss'
 
-export const SpotifyPage = observer(() => {
-
-  return <div className={styles.spotifyContainer}>
+export const SpotifyPage = observer(() => (
+  <div className={styles.spotifyContainer}>
     <iframe src="https://johneasterday.syrency.com/spotify-demo/"/>
   </div>
-});
+));
 
 export default SpotifyPage;
