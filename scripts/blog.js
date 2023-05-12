336d607351cfffe241ddad018fa0aa9c63810973
jmeaster30 <jmeasterday@comcast.net>
2023-05-12 00:18:08 -0400

    Fixed blog scrolling, started working on listing all the blog posts, removed the old big post and added a better one

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,6 +1,6 @@
 // newest to oldest
 const posts = [
-  { link: "the-artist-is-absent.html", title: "The Artist is Absent", author: "John Easterday", date: "3/9/2023" },
+  { link: "hotdogs-sandwiches.html", title: "A Hotdog is a Sandwich", author: "John Easterday", date: "5/11//2023" },
   { link: "testpost2.html", title: "This is ANOTHER test post", author: "John Easterday", date: "3/8/2023" },
   { link: "testpost.html", title: "This is a test post", author: "John Easterday", date: "3/8/2023" }
 ]
@@ -54,8 +54,43 @@ function prevPost() {
   loadPost(posts[prev_index]);
 }
 
+function showList() {
+  let listContainer = document.getElementById("post-list-container");
+  let contentContainer = document.getElementById("post-content-container");
+  let prevPostButton = document.getElementById('prev-post');
+  let nextPostButton = document.getElementById('next-post');
+  listContainer.dataset.shown = "yes";
+  contentContainer.dataset.shown = "no";
+  prevPostButton.dataset.turnOff = "yes";
+  nextPostButton.dataset.turnOff = "yes";
+
+  for (let i = 0; i < posts.length; i++) {
+    var entry = document.createElement('div');
+    entry.setAttribute("class", "post-list-item");
+    
+    var title = document.createElement('div');
+    title.className = "post-list-item-title";
+    title.innerHTML = posts[i].title;
+    entry.appendChild(title);
+
+    var author = document.createElement('span');
+    author.className = "post-list-item-author";
+    author.innerHTML = posts[i].author;
+    entry.appendChild(author);
+
+    var date = document.createElement('span');
+    date.className = "post-list-item-date";
+    date.innerHTML = posts[i].date;
+    entry.appendChild(date);
+
+    listContainer.appendChild(entry);
+  }
+
+}
+
 function initBlogPage() {
   loadPost(posts[0]);
   document.getElementById("prev-post").addEventListener('click', prevPost);
   document.getElementById("next-post").addEventListener('click', nextPost);
+  document.getElementById("list-post").addEventListener('click', showList);
 }
