029614b0c00e33091c109e791cc11e0555de48b2
jmeaster30 <jmeasterday@comcast.net>
2023-05-14 20:15:51 -0400

    Made the filters work and the github links

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,11 +2,11 @@ const projects_list = [
     {title: 'BlastPDF', description: 'Modify and generate PDF files utilizing a builder pattern for expressive pdf templates', gitLink: 'https://github.com/jmeaster30/BlastPDF', tags: ['C#', 'PDF', 'Code Generators']},
     {title: 'Ocean', description: 'A toy programming language that I am using for learning about compilers and development tooling', gitLink: 'https://github.com/jmeaster30/ocean', tags: ['Rust', 'Compiler']},
     {title: 'Noise Engine', description: 'An opengl game engine', gitLink: 'https://github.com/jmeaster30/noise-engine', tags: ['C++', 'OpenGL']},
-    {title: 'Spotify API Demo', description: 'Inspired by billclintonswag.com but with more people.', link: '/projects/spotifyswag', tags: ['TypeScript', 'SpotifyAPI','p5.js']},
+    {title: 'Spotify API Demo', description: 'Inspired by billclintonswag.com but with more people.', link: '/projects/spotifyswag', tags: ['Javascript', 'SpotifyAPI','p5.js']},
     {title: 'Stax', description: 'A concatenative stack-based programming language', gitLink: 'https://github.com/jmeaster30/stax', tags: ['Java', 'Compiler']},
     {title: 'SimpleDNS', description: 'A really basic DNS server', gitLink: 'https://github.com/jmeaster30/simpledns', tags: ['Rust', 'Networking']},
-    {title: 'Vore', description: 'A regex engine with english-like syntax', gitLink: 'https://github.com/jmeaster30/vore', link: '/projects/vore', tags: ['Go', 'Regex Engine']},
-    {title: 'Particle Life', description: 'A simulation of basic rules on a set of particles', link: '/projects/particlelife', tags: ['TypeScript', 'ThreeJS']},
+    {title: 'Vore', description: 'A regex engine with english-like syntax', gitLink: 'https://github.com/jmeaster30/vore', link: '/projects/vore', tags: ['Go', 'Regex Engine', 'WASM']},
+    {title: 'Particle Life', description: 'A simulation of basic rules on a set of particles', link: '/projects/particlelife', tags: ['Javascript', 'ThreeJS']},
     {title: 'PgPac', description: 'PostgreSQL deployment tool', gitLink: 'https://github.com/jmeaster30/pgpac', tags: ['PostgreSQL', 'Go', 'Database']},
     //{title: 'Pipe Layer', description: 'An HTML5 game based on the mobile game Flow', link: '/games/pipelayer', tags: ['Game', 'TypeScript', 'HTML5']}
 ];
@@ -24,9 +24,11 @@ function clickTag(event) {
     if (event.target.dataset.selected == "yes") {
         event.target.dataset.selected = "no";
         event.target.style.backgroundColor = "white";
+        buildProjects();
     } else {
         event.target.dataset.selected = "yes";
         event.target.style.backgroundColor = event.target.dataset.color;
+        buildProjects();
     }
 }
 
@@ -49,24 +51,45 @@ function buildTags() {
 }
 
 function buildProjects() {
+    console.log("build projects")
     let projectsListContainer = document.getElementById("projects-list");
     let tagListContainer = document.getElementById("projects-tags-list");
     let tagEntries = [];
     for(let i = 0; i < tagListContainer.children.length; i++) {
         let elem = tagListContainer.children[i];
-        tagEntries.push({tag: elem.innerHTML, color: elem.dataset.color});
+        tagEntries.push({tag: elem.innerHTML, color: elem.dataset.color, selected: elem.dataset.selected});
     }
-
-    let projects = projects_list.sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase());
-    for (let i = 0; i < projects.length; i++) {
+    let selectedTags = tagEntries.filter(x => x.selected == "yes");
+    console.log(selectedTags);
+    let projects = projects_list.filter(value => {
+        if (selectedTags.length == 0) return true;
+        for(let selectedTag of selectedTags) {
+            if (value.tags.includes(selectedTag.tag)) {
+                return true;
+            }
+        }
+        return false;
+    }).sort((a, b) => a.title.toLowerCase() > b.title.toLowerCase());
+    
+    projectsListContainer.innerHTML = "";
+    for (let project of projects) {
         let entry = document.createElement("div");
         entry.setAttribute("class", "projects-card");
-        entry.dataset.projectTitle = projects_list[i].title;
+        entry.dataset.projectTitle = project.title;
 
-        let title = document.createElement("div");
+        let title = document.createElement("span");
         title.setAttribute("class", "projects-card-title");
-        title.innerHTML = projects_list[i].title;
+        title.innerHTML = project.title;
         entry.appendChild(title);
+
+        if (project.gitLink) {
+            let gitIcon = document.createElement("a");
+            gitIcon.setAttribute("class", "projects-card-git-link");
+            gitIcon.setAttribute("href", project.gitLink);
+            gitIcon.setAttribute("target", "_blank");
+            gitIcon.innerHTML = "<img class=\"projects-card-git-icon\" src=\"assets/github-mark-white.svg\" alt=\"Github\"/>"
+            entry.appendChild(gitIcon);
+        }
 
         let divider = document.createElement("hr");
         divider.style.borderTop = "2px solid white";
@@ -74,19 +97,19 @@ function buildProjects() {
         
         let tagList = document.createElement("div");
         tagList.setAttribute("class", "projects-card-tags");
-        projects_list[i].tags = projects_list[i].tags.sort((a, b) => a.toLowerCase() > b.toLowerCase());
-        for (let j = 0; j < projects_list[i].tags.length; j++) {
+        project.tags = project.tags.sort((a, b) => a.toLowerCase() > b.toLowerCase());
+        for (let j = 0; j < project.tags.length; j++) {
             let tag = document.createElement("span");
             tag.setAttribute("class", "projects-card-tag");
-            tag.style.backgroundColor = tagEntries.find(x => x.tag == projects_list[i].tags[j])?.color || "#fff";
-            tag.innerHTML = projects_list[i].tags[j];
+            tag.style.backgroundColor = tagEntries.find(x => x.tag == project.tags[j])?.color || "#fff";
+            tag.innerHTML = project.tags[j];
             tagList.appendChild(tag);
         }
         entry.appendChild(tagList);
 
         let desc = document.createElement("div");
         desc.setAttribute("class", "projects-card-desc");
-        desc.innerHTML = projects_list[i].description;
+        desc.innerHTML = project.description;
         entry.appendChild(desc);
 
         projectsListContainer.appendChild(entry);
